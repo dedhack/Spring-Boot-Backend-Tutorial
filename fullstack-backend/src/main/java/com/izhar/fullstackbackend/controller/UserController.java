fb6d75e1ea15bf9359b3f1d5d07a8b3c4ccb41be
izharj <eezhar@gmail.com>
2023-03-31 11:25:38 +0800

    Update UserController.java

--- fullstack-backend/src/main/java/com/izhar/fullstackbackend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/izhar/fullstackbackend/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.izhar.fullstackbackend.controller;
 
 import com.izhar.fullstackbackend.model.User;
+import com.izhar.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserRepository {
+@CrossOrigin("http://localhost:5173") // allow front-end dev app to connect
+public class UserController {
     @Autowired
     private UserRepository userRepository;
 
